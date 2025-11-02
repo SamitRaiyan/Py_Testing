8679a0045ca8fb935c91b95b26b8a810bf574d9a
SamitRaiyan <samitr247@gmail.com>
2025-11-03 02:21:18 +0600

    set methods and examples updated

---
+++ setExample.py
@@ -0,0 +1,11 @@
+my_set = {1, 2, 3, 4, 6, 2, "apple", "banana"}
+print("Original set:", my_set) 
+
+# Empty set
+empty_set = set()
+print("Empty set:", empty_set)
+print("Type of empty set:", type(empty_set))
+
+# my_set.remove(2)  # Removes 2 from the set
+my_set.discard(2)  # Removes 2 from the set if it exists
+print("Set after removing 2:", my_set)
