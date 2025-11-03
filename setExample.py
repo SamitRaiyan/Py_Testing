09cd9a2338b381912c3539b9c4a25e6c75754359
SamitRaiyan <samitr247@gmail.com>
2025-11-03 18:50:31 +0600

    set methods

--- setExample.py
+++ setExample.py
@@ -6,6 +6,6 @@ empty_set = set()
 print("Empty set:", empty_set)
 print("Type of empty set:", type(empty_set))
 
-# my_set.remove(2)  # Removes 2 from the set
-my_set.discard(2)  # Removes 2 from the set if it exists
+# my_set.remove(2)   # Removes 2 from the set
+my_set.discard(2)    # Removes 2 from the set if it exists
 print("Set after removing 2:", my_set)
