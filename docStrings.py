1a117c74f74f75fa961be97fabf47772698f54a6
SamitRaiyan <samitr247@gmail.com>
2025-11-17 00:04:00 +0600

    example of docString

---
+++ docStrings.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    """
+    Add two numbers and return the result.
+    
+    Args:
+        a (int): First number
+        b (int): Second number
+    
+    Returns:
+        int: Sum of a and b
+    """
+    return a + b
+
+# Accessing docstring
+print(add.__doc__)
+help(add)
