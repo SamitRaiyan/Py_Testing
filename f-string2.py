97d31de02ca507a3a72b87daec8b37ce68411a7c
SamitRaiyan <samitr247@gmail.com>
2025-11-16 22:48:00 +0600

    f-string example

---
+++ f-string2.py
@@ -0,0 +1,7 @@
+name = "abc"
+area = "def"
+salary = 50.4568
+
+# print(f"Hello {name}, welcome to the {area}. Your salary is {salary:.2f} USD.")
+print(f"Name: {name}\nArea: {area}\nSalary: {salary:.2f} USD")
+print(type(f"{name}"))
