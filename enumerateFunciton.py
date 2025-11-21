7528adf656da53a912238804699ff685d617a835
SamitRaiyan <samitr247@gmail.com>
2025-11-21 10:43:01 +0600

    another commit

--- enumerateFunciton.py
+++ enumerateFunciton.py
@@ -1,4 +1,4 @@
-mark = [45,54,73,82,91,48,85,67]
+marks = [45,54,73,82,91,48,85,67]
 
-for index, value in enumerate(mark):
+for index, value in enumerate(marks):
     print(f"The mark of the student no {index+1}: ",value,f"while the index is {index}")
