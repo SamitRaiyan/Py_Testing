10b1ddd60023d32cd03b715d84375cd024760ab6
SamitRaiyan <samitr247@gmail.com>
2025-11-23 23:39:59 +0600

    file exercise done

--- file6.py
+++ file6.py
@@ -5,9 +5,9 @@ while True:
   line = f.readline()
   if not line:
     break
-  m1 = int(line.split(",")[0])
-  m2 = int(line.split(",")[1])
-  m3 = int(line.split(",")[2])
+  m1 = int(line.split(",")[0]) # splitting line at comma and converting to int
+  m2 = int(line.split(",")[1]) # splitting line at comma and converting to int
+  m3 = int(line.split(",")[2]) # splitting line at comma and converting to int
   print(f"Marks of student {i} in Maths is: {m1}")
   print(f"Marks of student {i} in English is: {m2}")
   print(f"Marks of student {i} in SST is: {m3}")
