bb77793810598a46b8f17ade90fe75cb70a1cf1b
SamitRaiyan <samitr247@gmail.com>
2025-11-26 12:59:08 +0600

    decorator example

---
+++ decorators.py
@@ -0,0 +1,19 @@
+def decorator_function(fx):  # fx = function to be decorated (main function as input)
+    def mfx(): # mfx = modified fx
+        print("--------------------------------------------------")
+        print("-------Going to execute the main function-------")
+        output =fx()  # Call the main function
+        print("-------Finished executing the main function-------")
+        print("--------------------------------------------------")
+        return output  # Return the result of the main function
+    return mfx  # Return the modified function inside the decorator function
+
+@decorator_function
+def add():
+    a = int(input("Enter first number: "))
+    b = int(input("Enter second number: "))
+    return a + b
+
+# calling the add function
+result = add()
+print("Result:", result)
