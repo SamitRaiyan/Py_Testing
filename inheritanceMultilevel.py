ba0592b9dfe11adaa449cc102663484d2aa3aa8d
SamitRaiyan <samitr247@gmail.com>
2025-11-28 12:26:11 +0600

    Inheritance examples

---
+++ inheritanceMultilevel.py
@@ -0,0 +1,22 @@
+class Grandfather:
+ 
+    def __init__(self, grandFatherName):
+        self.grandFatherName = grandFatherName
+ 
+ 
+class Father(Grandfather):
+    def __init__(self, fatherName, grandFatherName):
+        self.fatherName = fatherName
+        Grandfather.__init__(self, grandFatherName)
+class Son(Father):
+    def __init__(self, sonName, fatherName, grandFatherName):
+        self.sonName = sonName
+        Father.__init__(self, fatherName, grandFatherName)
+ 
+    def print_name(self):
+        print('Grandfather name :', self.grandFatherName)
+        print("Father name :", self.fatherName)
+        print("Son name :", self.sonName)
+s1 = Son('Prince', 'Rampal', 'Lal mani')
+print(s1.grandFatherName)
+s1.print_name()
---
+++ inheritanceMultiple.py
@@ -0,0 +1,20 @@
+class Mother:
+    motherName = ""
+ 
+    def mother(self):
+        print(self.motherName)
+ 
+class Father:
+    fatherName = ""
+ 
+    def father(self):
+        print(self.fatherName)
+ 
+class Son(Mother, Father):
+    def parents(self):
+        print("Father name is :", self.fatherName)
+        print("Mother :", self.motherName)
+s1 = Son()
+s1.fatherName = "Daddy"
+s1.motherName = "Mommy"
+s1.parents()
