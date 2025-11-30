d60177e7f1a116b7b74d0649ddf511c62a9da651
SamitRaiyan <samitr247@gmail.com>
2025-11-30 18:55:09 +0600

    example of shutil module

---
+++ shutilModule.py
@@ -0,0 +1,36 @@
+import shutil
+import os
+
+# shutil.copy("main.py", "main2.py") # copy file
+# shutil.copytree(".tutorial", "myTutorial") # copy directory
+# shutil.move(".tutorial/file.file", "file.file") # move file
+# shutil.rmtree("myTutorial") # delete directory
+os.remove("file.file") # delete file
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+import shutil
+
+# Copying a file
+shutil.copy("src.txt", "dst.txt")
+
+# Copying a directory
+shutil.copytree("src_dir", "dst_dir")
+
+# Moving a file
+shutil.move("src.txt", "dst.txt")
+
+# Deleting a directory
+shutil.rmtree("dir")
+
+'''
