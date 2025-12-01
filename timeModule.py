c4eafb9c602c1cb1cc42243f1f57834c029040ee
SamitRaiyan <samitr247@gmail.com>
2025-12-02 00:21:55 +0600

    a small example of time module

---
+++ timeModule.py
@@ -0,0 +1,5 @@
+import time # time module is a built-in Python module for time-related functions
+
+# for printing after 3 seconds
+time.sleep(3)
+print("This message is printed after a delay of 3 seconds.")
